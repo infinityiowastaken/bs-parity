eeb2b9b731a6041500919218d4a190d55e352cde
nathan <11426516+infinityiowastaken@users.noreply.github.com>
2020-06-29 16:04:24 +0100

    implementation of wall-finding code

--- ui.js
+++ ui.js
@@ -189,7 +189,9 @@ function readFile(files) {
     introDiv.classList.add('uploading');
     fr.readAsText(files[0]);
     fr.addEventListener('load', function () {
-        notesArray = getNotes(JSON.parse(fr.result));
+        parsed = JSON.parse(fr.result)
+        notesArray = getNotes(parsed);
+        wallsArray = getWalls(parsed);
         introDiv.classList.remove('uploading');
         introDiv.classList.add('done');
         console.log("successful read!");
@@ -216,4 +218,17 @@ function getNotes(obj) {
         return types[note._type] !== undefined;
     });
     return notes;
+}
+
+function getWalls(obj) {
+    let walls = obj._obstacles;
+    walls.sort(function (a, b) {
+        return a._time - b._time;
+    })
+
+    // filter out invalid note types
+    walls = walls.filter(function (wall) {
+        return (wall._width >= 1 && wall._duration >= 0)
+    });
+    return walls;
 }
